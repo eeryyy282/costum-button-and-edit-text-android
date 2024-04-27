92efe09c024bbc104f1e14586fb35c5935d8c028
Airi <airiagustus82@gmail.com>
2024-04-28 01:56:12 +0700

    fix some mistake code

--- app/src/main/java/com/example/costumbuttonandedittext/ui/MainActivity.kt
+++ app/src/main/java/com/example/costumbuttonandedittext/ui/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : AppCompatActivity() {
 
         myEditText.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-                TODO("Not yet implemented")
+
             }
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
@@ -41,7 +41,7 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun afterTextChanged(s: Editable?) {
-                TODO("Not yet implemented")
+
             }
 
         })
