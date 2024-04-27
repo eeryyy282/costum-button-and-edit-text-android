83a3421af5beba814dbe58e836585dbeb4d01684
Airi <airiagustus82@gmail.com>
2024-04-28 01:54:25 +0700

    add costume edit text to main activity

--- app/src/main/java/com/example/costumbuttonandedittext/ui/MainActivity.kt
+++ app/src/main/java/com/example/costumbuttonandedittext/ui/MainActivity.kt
@@ -1,13 +1,21 @@
 package com.example.costumbuttonandedittext.ui
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.costumbuttonandedittext.R
+import com.example.costumbuttonandedittext.ui.costum.MyButton
+import com.example.costumbuttonandedittext.ui.costum.MyEditText
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var myButton: MyButton
+    private lateinit var myEditText: MyEditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -17,5 +25,34 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        myButton = findViewById(R.id.my_button)
+        myEditText = findViewById(R.id.my_edit_text)
+
+        setMyButtonEnabled()
+
+        myEditText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                setMyButtonEnabled()
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                TODO("Not yet implemented")
+            }
+
+        })
+
+        myButton.setOnClickListener {
+            Toast.makeText(this, myEditText.text, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun setMyButtonEnabled() {
+        val result = myEditText.text
+        myButton.isEnabled = result != null && result.toString().isNotEmpty()
     }
 }
